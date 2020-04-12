bcd2eb80f5f1e7dabd1021dd8f99b94704c50d19
Jesus Alberto Acosta Heredia <info@xmediasoftware.com>
2020-04-12 00:34:36 -0700

    Revision - Fix cambia el formato si está equivocado
    Cuando el formato de la imagen o el archivo este es renombrado,

--- generate_thumb.php
+++ generate_thumb.php
@@ -17,9 +17,9 @@
 //	version: 1.0
 
 $thumb_max_width_default = 100;
-$msg_error_file_not_found = "Error: Please select a file to be uploaded!";
-$msg_error_file_format_worng = "incorrect File Type or not permitted, only these files are allowed";
-$msg_library_not_found = "Error: Please check that you have the GD library version 2+";
+$msg_error_file_not_found = "Error: Please select a file to be uploaded!<br>";
+$msg_error_file_format_worng = "incorrect File Type or not permitted, only these files are allowed<br>";
+$msg_library_not_found = "Error: Please check that you have the GD library version 2+<br>";
 
 // IfFileExistsRename ($file_name, $new_location)
 //
@@ -123,6 +123,104 @@ function getWidthFromPicture($file_name_location){
 	
 }
 
+/*
+
+Clase FileInfoTool
+
+gracias a ccbsschucko@gmail.com 
+
+
+
+*/
+
+
+class FileInfoTool {
+
+    /**
+    * @var str => $file = caminho para o arquivo (ABSOLUTO OU RELATIVO)
+    * @var arr => $file_info = array contendo as informações obtidas do arquivo informado
+    */
+    private $file;
+    private $file_info;
+
+    /**
+    * @param str => $file = caminho para o arquivo (ABSOLUTO OU RELATIVO)
+    */
+    public function get_file(string $file){
+        clearstatcache();
+        $file = str_replace(array('/', '\\'), array(DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR), $file);
+        if(!is_file($file) && !is_executable($file) && !is_readable($file)){
+            throw new \Exception('O arquivo informado não foi encontrado!');
+        }
+        $this->file = $file;
+        $this->set_file_info($this->file);
+        return $this;
+    }
+
+    /**
+    * @param str => $index = se for informado um indice é retornada uma informação específica do arquivo
+    */
+    public function get_info($index = ''){
+        if($this->get_file_is_called()){
+            if($index === ''){
+                return $this->file_info;
+            }
+            if($index != ''){
+                if(!array_key_exists($index, $this->file_info)){
+                    throw new \Exception('A informação requisitada não foi encontrada!');                   
+                }
+                return $this->file_info;
+            }
+        }
+    }
+
+    /**
+    * @todo verifica se o método get_file() foi utilizado para informar o caminho do arquivo
+    */
+    private function get_file_is_called(){
+        if(!$this->file){
+            throw new \Exception('Nenhum arquivo foi fornecido para análise. Utilize o método get_file() para isso!');
+            return false;
+        }
+        return true;
+    }
+
+    /**
+    * @todo preencher a array com as infos do arquivo
+    */
+    private function set_file_info(){
+        $this->file_info = array();
+        $pathinfo = pathinfo($this->file);
+        $stat = stat($this->file);
+        $this->file_info['realpath'] = realpath($this->file);
+        $this->file_info['dirname'] = $pathinfo['dirname'];
+        $this->file_info['basename'] = $pathinfo['basename'];
+        $this->file_info['filename'] = $pathinfo['filename'];
+        $this->file_info['extension'] = $pathinfo['extension'];
+        $this->file_info['mime'] = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $this->file);
+        $this->file_info['encoding'] = finfo_file(finfo_open(FILEINFO_MIME_ENCODING), $this->file);
+        $this->file_info['size'] = $stat[7];
+        $this->file_info['size_string'] = $this->format_bytes($stat[7]);
+        $this->file_info['atime'] = $stat[8];
+        $this->file_info['mtime'] = $stat[9];
+        $this->file_info['permission'] = substr(sprintf('%o', fileperms($this->file)), -4);
+        $this->file_info['fileowner'] = getenv('USERNAME');
+    }
+
+    /**
+    * @param int => $size = valor em bytes a ser formatado
+    */
+    private function format_bytes(int $size){
+        $base = log($size, 1024);
+        $suffixes = array('', 'KB', 'MB', 'GB', 'TB');  
+        return round(pow(1024, $base-floor($base)), 2).''.$suffixes[floor($base)];
+    }
+}
+
+
+
+
+
 
 //
 //
@@ -138,9 +236,12 @@ function UploadPictureTumbnail($posted_picture, $new_location, $overwrite, $img_
 	$file_name ="";
 	global $thumb_max_width_default;
 	global $msg_error_file_not_found;
+	$error    = $posted_picture['error'];
+	$size     = $posted_picture['size'];
+	$phisical_ext      = strtolower(pathinfo($posted_picture['name'], PATHINFO_EXTENSION));
 
 	if(substr($new_location,-1,1) != "/"){
-		$new_location += $new_location . "/";
+		$new_location = $new_location . "/";
 	}
 	
 	if($img_thumb_width < $thumb_max_width_default){
@@ -155,30 +256,99 @@ function UploadPictureTumbnail($posted_picture, $new_location, $overwrite, $img_
 	$file_name = $posted_picture['name'];
 	$file_size = $posted_picture['size'];
 	$file_tmp = $posted_picture['tmp_name'];
+
+	// Correccion, en caso de que exista finfo se utiliz pra detectar el tipo de imagen
+	//if(extension_loaded("file_info")){
+		$all_new_fileinfo = (new FileInfoTool)->get_file($file_tmp)->get_info();
+		$file_type = $all_new_fileinfo['mime'];
+		$ext = explode ("/", $all_new_fileinfo['mime'])[1];
+		echo "Si existe finfo	<br>";
+	//}
+
+	echo "Tipo de Archivo: " . $file_type . "<br>";
+	echo "Extension: " . $ext . "<br>";
+	echo "Extension Fisica: " . $phisical_ext . "<br>";
+	/////////////////////////////////////////
+
+	$original_filename =  $posted_picture['name']; 
+
+	echo "Nombre de e archivo original: " . $original_filename . "<br>";
 	
 	if(!$overwrite){
-		$file_name = IfFileExistsRename($posted_picture['name'], $new_location);
+		$file_name = IfFileExistsRename($original_filename,  $new_location);
 	}else{
 		$file_name = $posted_picture['name'];
 	}
+
+	$file_name = str_replace($phisical_ext, $ext, $file_name);
+
+	echo "Nuevo nombre de archivo: " . $file_name . "<br>";
 	
+
+	switch ($error) {
+        case UPLOAD_ERR_OK:
+            $valid = true;
+            //validate file extensions
+            if ( !in_array($ext, $limitedext) ) {
+            	global $msg_error_file_format_worng;
+                $valid = false;
+                $response = $msg_error_file_format_worng . '<br>';
+            }
+            //validate file size
+            if ( $size/1024/1024 > 2 ) {
+                $valid = false;
+                $response = 'File size is exceeding maximum allowed size.<br>';
+            }
+            //upload file
+            if ($valid) {
+                continue;
+                exit;
+            }
+            break;
+        case UPLOAD_ERR_INI_SIZE:
+            $response = 'The uploaded file exceeds the upload_max_filesize directive in php.ini.<br>';
+            break;
+        case UPLOAD_ERR_FORM_SIZE:
+            $response = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form.<br>';
+            break;
+        case UPLOAD_ERR_PARTIAL:
+            $response = 'The uploaded file was only partially uploaded.<br>';
+            break;
+        case UPLOAD_ERR_NO_FILE:
+            $response = 'No file was uploaded.<br>';
+            break;
+        case UPLOAD_ERR_NO_TMP_DIR:
+            $response = 'Missing a temporary folder. Introduced in PHP 4.3.10 and PHP 5.0.3.<br>';
+            break;
+        case UPLOAD_ERR_CANT_WRITE:
+            $response = 'Failed to write file to disk. Introduced in PHP 5.1.0.<br>';
+            break;
+        case UPLOAD_ERR_EXTENSION:
+            $response = 'File upload stopped by extension. Introduced in PHP 5.2.0.<br>';
+            break;
+        default:
+            $response = 'Unknown error<br>';
+        break;
+    }
+
+
+
+
+/*
+	echo "Revisando si se sube: " . $file_tmp . "<br>";
 	if(!is_uploaded_file($file_tmp)){
 	echo $msg_error_file_not_found;
 	exit(1); //exit the script and don't process the rest of it!
-	}
+	}*/
 
 	//check the file's extension
-	$ext = strrchr($file_name,'.');
-	$ext = strtolower($ext);
+	/*$ext = strrchr($file_name,'.');
+	$ext = strtolower($ext);*/
 
-	if (($extlimit == "yes") && (!in_array($ext,$limitedext))) {
-		echo  $msg_error_file_format_worng ." " . implode (", ",$limitedext);
-		exit();
-	}
 
 	//so, whats the file's extension?
-	$getExt = explode ('.', $file_name);
-	$file_ext = $getExt[count($getExt)-1];
+	/*$getExt = explode ('.', $file_name);
+	$file_ext = $getExt[count($getExt)-1];*/
 
 	//the new width variable
 	$ThumbWidth = $img_thumb_width;
@@ -189,7 +359,7 @@ function UploadPictureTumbnail($posted_picture, $new_location, $overwrite, $img_
 	
 	//keep image type
 	if($file_size){
-		if($file_type == "image/pjpeg" || $file_type == "image/jpeg"){
+		if($file_type == "image/jpg" || $file_type == "image/jpeg"){
 			$new_img = imagecreatefromjpeg($file_tmp);
 		}elseif($file_type == "image/x-png" || $file_type == "image/png"){
 			$new_img = imagecreatefrompng($file_tmp);
